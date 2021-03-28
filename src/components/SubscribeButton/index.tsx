5da82f25f9263e62fa03d39a9256f3d4acff0ea2
MoesioMarcelino <moesiomarcelino1@gmail.com>
2021-03-28 19:35:03 -0300

    Redirect with stripe, checkout session and block duplicate criation of stripe's customers

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,47 @@
+import { useSession, signIn } from 'next-auth/client';
+import { getStripeJS } from '../../services/stripe-js';
+
+import { api } from '../../services/api';
+
 import styles from './styles.module.scss';
+import Stripe from 'stripe';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+interface StripeResponse extends Stripe.Response<Stripe.Checkout.Session> {
+  sessionId: string;
+}
+
 export function SubscribeButton({}: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const {
+        data: { sessionId },
+      } = await api.post<StripeResponse>('/subscribe');
+
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch ({ message }) {
+      alert(message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
